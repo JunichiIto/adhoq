c3dad3907f9e488c9907d6c629782aba709f1530
Junichi Ito <jit@sonicgarden.jp>
2026-02-18 20:03:53 +0900

    Remove ActiveSupport::Configurable since it's deprecated since Rails 8.1

--- lib/adhoq/configuration.rb
+++ lib/adhoq/configuration.rb
@@ -1,30 +1,18 @@
 # see https://github.com/amatsuda/kaminari/blob/master/lib/kaminari/config.rb
 module Adhoq
   class Configuration
-    include ActiveSupport::Configurable
+    attr_accessor :storage, :base_controller, :authorization, :authorization_failure_action,
+                  :current_user, :database_connection, :hidden_model_names, :hide_rows_count,
+                  :async_execution, :job_queue_name
 
-    config_accessor :storage do
-      [:on_the_fly]
+    def initialize
+      @storage = [:on_the_fly]
+      @base_controller = 'ApplicationController'
     end
-
-    config_accessor :base_controller do
-      'ApplicationController'
-    end
-
-    config_accessor :authorization
-    config_accessor :authorization_failure_action
-
-    config_accessor :current_user
-
-    config_accessor :database_connection
-    config_accessor :hidden_model_names
-    config_accessor :hide_rows_count
-
-    config_accessor :async_execution
-    config_accessor :job_queue_name
 
     def callablize(name)
-      if (c = config[name]).respond_to?(:call)
+      c = send(name)
+      if c.respond_to?(:call)
         c
       else
         c.to_proc
